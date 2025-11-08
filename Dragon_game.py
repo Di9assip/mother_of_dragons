89179cee7834e48f860d382edd79c6092a3b6f99
Margarita Kuznetsova <ritok2609@gmail.com>
2025-11-08 19:06:56 +0300

    Moved requirements.txt to root

--- Dragon_game.py
+++ Dragon_game.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template , request
+from flask import Flask, render_template, request
+import os
+
 app = Flask(__name__)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     message = None
     if request.method == 'POST':
         action = request.form['action']
-        if action == "attack":
+        if action == 'attack':
             message = "Ты сразилась с драконом и победила!"
-        elif action == "run":
+        elif action == 'run':
             message = "Ты убежала от дракона. Он остался доволен и не тронул тебя."
     return render_template('index.html', message=message)
-if __name__ == '__main__':
-    import os
 
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
