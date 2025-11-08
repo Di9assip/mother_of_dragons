6925ce80087ff8d569732a41410cd1e85a2958a7
Margarita Kuznetsova <ritok2609@gmail.com>
2025-11-08 18:24:16 +0300

    fix flask app

--- Dragon_game.py
+++ Dragon_game.py
@@ -11,4 +11,7 @@ def index():
             message = "Ты убежала от дракона. Он остался доволен и не тронул тебя."
     return render_template('index.html', message=message)
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
